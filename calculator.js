b459d622eeb1b114972897e9c855896079a32c9a
Paycrest Admin <128632949+paycrest-dev@users.noreply.github.com>
2025-02-22 00:40:42 -0800

    feat: added sub and mul

--- calculator.js
+++ calculator.js
@@ -8,7 +8,19 @@ class Calculator {
     this.history.push({ operation: "add", numbers, result });
     return result;
   }
-    
+        subtract(...numbers) {
+        const result = numbers.reduce((diff, num, idx) => 
+            idx === 0 ? num : diff - num);
+        this.history.push({ operation: 'subtract', numbers, result });
+        return result;
+    }
+
+    multiply(...numbers) {
+        const result = numbers.reduce((product, num) => product * num, 1);
+        this.history.push({ operation: 'multiply', numbers, result });
+        return result;
+    }
+
   // TODO: Implement the other operations like sub, mul, div
 }
 
