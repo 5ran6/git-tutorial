2671ce80823b247a3d0874101434340f4fb645ea
Francis Ocholi <5raan6@gmail.com>
2025-02-22 00:33:08 -0800

    feat: addition method

--- calculator.js
+++ calculator.js
@@ -1,1 +1,16 @@
-random stuff
+class Calculator {
+  constructor() {
+    this.history = [];
+  }
+
+  add(...numbers) {
+    const result = numbers.reduce((sum, num) => sum + num, 0);
+    this.history.push({ operation: "add", numbers, result });
+    return result;
+  }
+    
+  // TODO: Implement the other operations like sub, mul, div
+}
+
+const calc = new Calculator();
+console.log(calc.add(1, 2, 3));  // 6
